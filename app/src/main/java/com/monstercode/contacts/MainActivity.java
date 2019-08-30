80f899339392db5ab787bd1b6b183045e7ddaf09
rsgilbert <gssenyonjo@gmail.com>
2019-08-30 12:24:34 +0300

    Implemented the Search Dialog. moving on to suggestions

--- app/src/main/java/com/monstercode/contacts/MainActivity.java
+++ app/src/main/java/com/monstercode/contacts/MainActivity.java
@@ -1,87 +1,94 @@
 package com.monstercode.contacts;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ListActivity;
+import android.app.SearchManager;
 import android.content.ContentResolver;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static android.app.Activity.DEFAULT_KEYS_SEARCH_LOCAL;
+
 public class MainActivity extends AppCompatActivity {
     public ListView contacts_listview;
     public static final String TAG = "mainactivity";
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "oncreate: starting activity main");
         setContentView(R.layout.activity_main);
-
         contacts_listview = findViewById(R.id.contacts_listview);
 
         String[] projection = {
-          ContactsContract.Columns._ID,
           ContactsContract.Columns.CONTACTS_NAME,
-          ContactsContract.Columns.CONTACTS_CONTACT,
-          ContactsContract.Columns.CONTACTS_POSITION,
-          ContactsContract.Columns.CONTACTS_MINISTRY
+          ContactsContract.Columns.CONTACTS_CONTACT
         };
+//        String selection = ContactsContract.Columns.CONTACTS_NAME + " LIKE 'mu%'";
+        Cursor cursor = getContentResolver().query(ContactsContract.CONTENT_URI,
+                null,
+                null,
+                    null,
+                ContactsContract.Columns.CONTACTS_NAME
+        );
 
-        ContentResolver contentResolver = getContentResolver();
-
-
-        // manipulating appDb
-        ContentValues contentValues = new ContentValues();
-        contentValues.put(ContactsContract.Columns.CONTACTS_NAME, "Geofrey Ssenyondo");
-        contentValues.put(ContactsContract.Columns.CONTACTS_CONTACT, "0733333333");
-        contentValues.put(ContactsContract.Columns.CONTACTS_POSITION, "Chairman");
-        contentValues.put(ContactsContract.Columns.CONTACTS_MINISTRY, "Ministry of Internal Affairs");
-
-//        contentResolver.insert(ContactsContract.CONTENT_URI, contentValues);
-
-
-        Cursor cursor = contentResolver.query(ContactsContract.CONTENT_URI,
+        ListAdapter adapter = new SimpleCursorAdapter(
+                this,
+                android.R.layout.two_line_list_item,
+                cursor,
                 projection,
-                null,
-                null,
-                ContactsContract.Columns.CONTACTS_NAME);
+                new int[] {android.R.id.text1, android.R.id.text2}
+                );
 
-        if (cursor != null) {
-            final List<String> contact_names = new ArrayList<>();
-            final List<Integer> contact_ids = new ArrayList<>();
-            while (cursor.moveToNext()) {
-                contact_names.add(cursor.getString(cursor.getColumnIndex(ContactsContract.Columns.CONTACTS_NAME)));
-                contact_ids.add(cursor.getInt(cursor.getColumnIndex(ContactsContract.Columns._ID)));
+        contacts_listview.setAdapter(adapter);
 
+    }
 
-            }
-            cursor.close();
-            ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, R.layout.contact_detail, R.id.contact_item, contact_names);
-            contacts_listview.setAdapter(adapter);
-            contacts_listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                    int _id =  contact_ids.get(i);
-                    Log.d(TAG, "contactlist item id: " + _id);
-                    Uri uri = ContactsContract.buildContactUri(_id);
-                    Log.d(TAG, "New uri is " + uri);
-                    Intent intent = new Intent(MainActivity.this, InfoActivity.class);
-                    intent.setData(uri);
-                    startActivity(intent);
-                }
-            });
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.menu_search) {
+            onSearchRequested();
         }
+        return super.onOptionsItemSelected(item);
     }
 }
 
-// \
+
+
+    // get the SearchView and set searchable configuration
+//    SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+//    SearchView searchView = (SearchView) menu.findItem(R.id.menu_search).getActionView();
+//
+//// current activity is the searchable activity
+//        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+//                searchView.setIconifiedByDefault(true); // expand the widget by default
+//                searchView.setSubmitButtonEnabled(true);
---
+++ app/src/main/java/com/monstercode/contacts/SearchableActivity.java
@@ -0,0 +1,86 @@
+package com.monstercode.contacts;
+
+import android.app.ListActivity;
+import android.app.SearchManager;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class SearchableActivity extends AppCompatActivity {
+    public static final String TAG = "SearchableActivity";
+    public ListView contacts_listview;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        contacts_listview = findViewById(R.id.contacts_listview);
+        Log.d(TAG, "oncreate, starting search");
+
+        // get intent, verify action, use query
+        Intent intent = getIntent();
+        if(Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+
+            Log.d(TAG, "oncreate, making query" + query);
+
+            Cursor matchesCursor = getMatches(query);
+
+            ListAdapter adapter = new SimpleCursorAdapter(
+                    this,
+                    android.R.layout.two_line_list_item,
+                    matchesCursor,
+                    new String[] { ContactsContract.Columns.CONTACTS_NAME,
+                        ContactsContract.Columns.CONTACTS_CONTACT },
+                    new int[] { android.R.id.text1, android.R.id.text2 }
+            );
+            contacts_listview.setAdapter(adapter);
+        }
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.menu_search) {
+            onSearchRequested();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    public Cursor getMatches(String query) {
+        String selection = ContactsContract.Columns.CONTACTS_NAME + " LIKE ?";
+//                + ContactsContract.Columns.CONTACTS_CONTACT + " MATCH ?OR"
+//                + ContactsContract.Columns.CONTACTS_POSITION + " MATCH ? OR"
+//                + ContactsContract.Columns.CONTACTS_MINISTRY + " MATCH ?;";
+
+
+        return getContentResolver().query(ContactsContract.CONTENT_URI,
+                null,
+                selection,
+                new String[]{"%" + query + "%"},
+                ContactsContract.Columns.CONTACTS_NAME);
+    }
+}
